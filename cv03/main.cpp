9911184388be2edd59e5de07423d8fdbb7fdb3cd
Petr Laštovička <lastope2@fit.cvut.cz>
2022-02-28 20:21:02 +0100

    cv03: Finished

--- cv03/main.cpp
+++ cv03/main.cpp
@@ -14,56 +14,61 @@
 using namespace std;
 #endif /* __PROGTEST__ */
 
+#define DPRINT if (true)
+
+
 struct Contact {
-	size_t order;
 	string firstName, lastName, number;
 };
 
 bool openFile(const string &fileName, ifstream &in);
 bool readLines(ifstream &in, vector<Contact> &vector);
-bool checkPhoneNumber(string &number);
-
+bool checkPhoneNumber(const string &number);
+bool search(ostream & out, ifstream & in, const vector<Contact> & vector);
+void printForQuery(ostream & out, const string & query, const vector<Contact> & vector);
 
 
 bool report(const string &fileName, ostream &out) {
 
+	DPRINT cout << "\n\nWorking on " << fileName << "\n" << endl;
 	// opening file
 	ifstream in;
 	bool succeeded = openFile(fileName, in);
-#ifdef DEBUG
-	cout << "File opened: " << succeeded << endl;
-#endif
-	if (!succeeded)
-		return false;
+	DPRINT cout << "File opened: " << succeeded << endl;
+	if (!succeeded) return false;
 
 	vector<Contact> contacts;
 	if (!readLines(in, contacts))
 	{
-#ifdef DEBUG
-		cout << "Reading error" << endl;
-#endif
+		DPRINT cout << "Reading error" << endl;
+		in.close();
 		return false;
 	}
 
+#ifdef DEBUG
+	cout << "Contacts read:" << endl;
 	for (auto &c : contacts) {
-		cout << c.order << " " << c.firstName << " " << c.lastName << " " << c.number << "\n";
+		cout << c.firstName << " " << c.lastName << " " << c.number << "\n";
 	}
-	cout << flush;
+	cout << endl;
+#endif
+
+	bool searchResult = search(out, in, contacts);
 
 	in.close();
-	return true;
+	return searchResult;
 }
 
 bool openFile(const string &fileName, ifstream &in) {
 	in = ifstream(fileName, ios::in);
-	return in.is_open();
+	return in.is_open() && in.good();
 }
 
-bool readLines(ifstream &in, vector<Contact> &vector) {
+bool readLines(ifstream & in, vector<Contact> & vector) {
 	string line;
 	stringstream str;
 
-	for (size_t order = 0; in; order++) {
+	while (in) {
 		getline(in, line);
 		if (line.length() == 0)
 			break;
@@ -73,25 +78,21 @@ bool readLines(ifstream &in, vector<Contact> &vector) {
 		str.str(line);
 		str >> first >> last >> number;
 		if (first.length() == 0 || last.length() == 0 || !checkPhoneNumber(number)) {
-#ifdef DEBUG
-			cout << "Remaining found" << endl;
-#endif
+			DPRINT cout << "Remaining found" << endl;
 			return false;
 		}
 
 		str >> remaining;
 		if (remaining.length() != 0) {
-#ifdef DEBUG
-			cout << "Remaining found" << endl;
-#endif
+			DPRINT cout << "Remaining found" << endl;
 			return false;
 		}
-		vector.push_back({order, first, last, number});
+		vector.push_back({first, last, number});
 	}
 	return in.good();
 }
 
-bool checkPhoneNumber(string &number) {
+bool checkPhoneNumber(const string &number) {
 	if (number.length() != 9)
 		return false;
 	for (auto c : number)
@@ -99,6 +100,48 @@ bool checkPhoneNumber(string &number) {
 			return false;
 	return number.at(0) != '0';
 }
+
+bool search(ostream & out, ifstream & in, const vector<Contact> & vector) {
+	for (; in; ) {
+		//auto peaked = in.peek();
+	 	//cout << "Peaked " << peaked << " (" << (int) peaked << ")" << endl;
+		in.peek();
+		if (in.eof()) break;
+
+		string line = "", query = "";
+
+		getline(in, line);
+		stringstream str(line);
+		str >> query;
+
+		//cout << "line: '" << line << "' query: '" << query << "'" << endl;
+		//cout << "good: " << in.good() << " eof: " << in.eof() << endl;
+		//cout << "good: " << str.good() << " eof: " << str.eof() << endl;
+
+		if (in.good() || in.eof())
+			printForQuery(out, query, vector);
+	}
+	return in.good() || in.eof();
+}
+
+void printForQuery(ostream & out, const string & query, const vector<Contact> & vector) {
+	DPRINT cout << "Quering: '" << query << "'" << endl;
+	size_t counter = 0;
+	for (auto & c : vector) {
+		if (c.firstName.compare(query) == 0 || c.lastName.compare(query) == 0 ) {
+			counter++;
+			out << c.firstName << " " << c.lastName << " " << c.number << "\n";
+			DPRINT cout << c.firstName << " " << c.lastName << " " << c.number << endl;
+		}
+	}
+	out << "-> " << counter << endl;
+	DPRINT cout << "-> " << counter << endl;
+}
+
+
+
+
+
 
 #ifndef __PROGTEST__
 int main() {
@@ -117,6 +160,27 @@ int main() {
 		   "-> 1\n");
 	oss.str("");
 	assert(report("tests/test1_in.txt", oss) == false);
+
+	oss.str("");
+	assert(report("tests/progtest0.txt", oss));
+	assert(oss.str() ==
+			"Carly Johnson 355562144\n"
+		    "-> 1\n"
+			"Christen Day 711567172\n"
+		    "-> 1\n");
+
+	oss.str("");
+	assert(report("tests/progtest1.txt", oss));
+	assert(oss.str() ==
+		   "John Christescu 258452362\n"
+		   "John Christescu 258452362\n"
+		   "John Christescu 258452362\n"
+		   "-> 3\n"
+		   "-> 0\n"
+		   "-> 0\n"
+		   "-> 0\n"
+		   "-> 0\n");
+
 	return 0;
 }
 #endif /* __PROGTEST__ */
