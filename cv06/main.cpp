6cba053e8a46211e285381d80e0ca37747cc5476
Petr Laštovička <lastope2@fit.cvut.cz>
2022-03-22 19:28:08 +0100

    cv06: Fixed broken Remove method

--- cv06/main.cpp
+++ cv06/main.cpp
@@ -1,5 +1,7 @@
 #ifndef __PROGTEST__
 #include <cstring>
+#include <iostream>
+#include <iomanip>
 using namespace std;
 
 class CLinkedSetTester;
@@ -27,7 +29,7 @@ class CLinkedSet {
         size_t m_Size = 0;
     public:
         CLinkedSet() {}
-        CLinkedSet(const CLinkedSet &  set) {
+        CLinkedSet(const CLinkedSet & set) {
             CNode * node = set.m_Begin;
             CNode ** target = &m_Begin;
             while(node != nullptr) {
@@ -45,10 +47,12 @@ class CLinkedSet {
                 delete node;
                 node = next;
             }
+            m_Begin = nullptr;
+            m_Size = 0;
         }
         CLinkedSet & operator=(CLinkedSet set) {
             {
-                CNode * tmp = m_Begin;
+                CNode * const tmp = m_Begin;
                 m_Begin = set.m_Begin;
                 set.m_Begin = tmp;
             }
@@ -67,9 +71,9 @@ class CLinkedSet {
                 return true;
             }
             {
-                int cmp = strcmp(m_Begin -> Value(), value);
+                int cmp = strcmp(value, m_Begin -> Value());
                 if (cmp == 0) return false;
-                if (cmp > 0) {
+                if (cmp < 0) {
                     CNode * old = m_Begin;
                     m_Begin = new CNode(value);
                     m_Begin -> m_Next = old;
@@ -80,9 +84,9 @@ class CLinkedSet {
             CNode * node = m_Begin -> m_Next;
             CNode ** prev = &m_Begin;
             while(node != nullptr) {
-                int cmp = strcmp(node -> Value(), value);
+                int cmp = strcmp(value, node -> Value());
                 if (cmp == 0) return false;
-                if (cmp > 0) {
+                if (cmp < 0) {
                     CNode * newNode = new CNode(value);
                     (*prev) -> m_Next = newNode;
                     newNode -> m_Next = node;
@@ -98,21 +102,23 @@ class CLinkedSet {
         }
         bool Remove ( const char * value ) {
             if (m_Size == 0) return false;
-            if (m_Size == 1) {
-                int cmp = strcmp(m_Begin -> Value(), value);
-                if (cmp != 0) return false;
-                delete m_Begin;
-                m_Begin = nullptr;
-                m_Size--;
-                return true;
+            {
+                int cmp = strcmp(value, m_Begin -> Value());
+                if (cmp == 0) {
+                    CNode * next = m_Begin -> m_Next;
+                    delete m_Begin;
+                    m_Begin = next;
+                    m_Size--;
+                    return true;
+                }
             }
             CNode * node = m_Begin -> m_Next;
             CNode ** prev = &m_Begin;
             while(node != nullptr) {
-                int cmp = strcmp(node -> Value(), value);
+                int cmp = strcmp(value, node -> Value());
+                // cout << "Comparing P: " << (*prev) -> Value() << " and N: " << node -> Value() << endl;
                 if (cmp == 0) {
-                    CNode * next = node -> m_Next;
-                    (*prev) -> m_Next = next;
+                    (*prev) -> m_Next = node -> m_Next;
                     delete node;
                     m_Size--;
                     return true;
@@ -135,12 +141,73 @@ class CLinkedSet {
         size_t Size () const { return m_Size; }
 
         friend class ::CLinkedSetTester;
+        friend void printLinkedSet(const CLinkedSet & set);
 };
 
 #ifndef __PROGTEST__
 #include <cassert>
 
+void printLinkedSet(const CLinkedSet & set) {
+    CLinkedSet::CNode * node = set.m_Begin;
+    while (node) {
+        cout << node -> Value() << endl;
+        node = node -> m_Next;
+    }
+    cout << setw(42) << setfill('-') << "" << endl;
+}
+
 struct CLinkedSetTester {
+    static void mTest0() {
+        CLinkedSet x0;
+        assert( x0.Insert( "1" ) );
+        assert( x0.Insert( "2" ) );
+        assert( x0.Insert( "3" ) );
+        assert( x0.Insert( "4" ) );
+        assert( x0.Insert( "5" ) );
+        assert( x0.Remove( "1" ) );
+        printLinkedSet(x0);
+    }
+    static void mTest1() {
+        CLinkedSet x0;
+        assert( x0.Insert( "1" ) );
+        assert( x0.Insert( "2" ) );
+        assert( x0.Insert( "3" ) );
+        assert( x0.Insert( "4" ) );
+        assert( x0.Insert( "5" ) );
+        assert( x0.Remove( "2" ) );
+        printLinkedSet(x0);
+    }
+    static void mTest2() {
+        CLinkedSet x0;
+        assert( x0.Insert( "1" ) );
+        assert( x0.Insert( "2" ) );
+        assert( x0.Insert( "3" ) );
+        assert( x0.Insert( "4" ) );
+        assert( x0.Insert( "5" ) );
+        assert( x0.Remove( "3" ) );
+        printLinkedSet(x0);
+    }
+    static void mTest3() {
+        CLinkedSet x0;
+        assert( x0.Insert( "1" ) );
+        assert( x0.Insert( "2" ) );
+        assert( x0.Insert( "3" ) );
+        assert( x0.Insert( "4" ) );
+        assert( x0.Insert( "5" ) );
+        assert( x0.Remove( "4" ) );
+        printLinkedSet(x0);
+    }
+    static void mTest4() {
+        CLinkedSet x0;
+        assert( x0.Insert( "1" ) );
+        assert( x0.Insert( "2" ) );
+        assert( x0.Insert( "3" ) );
+        assert( x0.Insert( "4" ) );
+        assert( x0.Insert( "5" ) );
+        assert( x0.Remove( "5" ) );
+        printLinkedSet(x0);
+    }
+
     static void test0() {
         CLinkedSet x0;
         assert( x0.Insert( "Jerabek Michal" ) );
@@ -152,21 +219,26 @@ struct CLinkedSetTester {
         assert( x0.Remove( "Jerabek Michal" ) );
         assert(!x0.Remove( "Pavlik Jan" ) );
         assert(!x0.Empty() );
+        printLinkedSet(x0);
     }
-
     static void test1() {
         CLinkedSet x0;
         assert( x0.Insert( "Jerabek Michal" ) );
         assert( x0.Insert( "Pavlik Ales" ) );
         assert( x0.Insert( "Dusek Zikmund" ) );
+        printLinkedSet(x0);
         assert( x0.Size() == 3 );
         assert( x0.Contains( "Dusek Zikmund" ) );
         assert(!x0.Contains( "Pavlik Jan" ) );
         assert( x0.Remove( "Jerabek Michal" ) );
         assert(!x0.Remove( "Pavlik Jan" ) );
         CLinkedSet x1 ( x0 );
+        printLinkedSet(x0);
+        printLinkedSet(x1);
         assert( x0.Insert( "Vodickova Saskie" ) );
         assert( x1.Insert( "Kadlecova Kvetslava" ) );
+        printLinkedSet(x0);
+        printLinkedSet(x1);
         assert( x0.Size() == 3 );
         assert( x1.Size() == 3 );
         assert( x0.Contains( "Vodickova Saskie" ) );
@@ -174,21 +246,25 @@ struct CLinkedSetTester {
         assert(!x0.Contains( "Kadlecova Kvetslava" ) );
         assert( x1.Contains( "Kadlecova Kvetslava" ) );
     }
-
     static void test2() {
         CLinkedSet x0;
         CLinkedSet x1;
         assert( x0.Insert( "Jerabek Michal" ) );
         assert( x0.Insert( "Pavlik Ales" ) );
         assert( x0.Insert( "Dusek Zikmund" ) );
+        printLinkedSet(x0);
         assert( x0.Size() == 3 );
         assert( x0.Contains( "Dusek Zikmund" ) );
         assert(!x0.Contains( "Pavlik Jan" ) );
         assert( x0.Remove( "Jerabek Michal" ) );
         assert(!x0.Remove( "Pavlik Jan" ) );
         x1 = x0;
+        printLinkedSet(x0);
+        printLinkedSet(x1);
         assert( x0.Insert( "Vodickova Saskie" ) );
         assert( x1.Insert( "Kadlecova Kvetslava" ) );
+        printLinkedSet(x0);
+        printLinkedSet(x1);
         assert( x0.Size() == 3 );
         assert( x1.Size() == 3 );
         assert( x0.Contains( "Vodickova Saskie" ) );
@@ -199,6 +275,12 @@ struct CLinkedSetTester {
 };
 
 int main () {
+    CLinkedSetTester::mTest0();
+    CLinkedSetTester::mTest1();
+    CLinkedSetTester::mTest2();
+    CLinkedSetTester::mTest3();
+    CLinkedSetTester::mTest4();
+
     CLinkedSetTester::test0();
     CLinkedSetTester::test1();
     CLinkedSetTester::test2();
