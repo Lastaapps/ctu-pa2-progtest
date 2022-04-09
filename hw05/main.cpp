f33f0622a5ed85470864ed2669f15bcfbcdd5ddc
Petr Laštovička <lastope2@fit.cvut.cz>
2022-04-09 22:35:59 +0200

    hw05: Selling working, ignoring transaction scheme

--- hw05/main.cpp
+++ hw05/main.cpp
@@ -20,6 +20,7 @@
 using namespace std;
 #endif /* __PROGTEST__ */
 
+#define INT_MAX 0x7FFFFFFF
 class CDate {
     private:
         uint16_t mY;
@@ -54,7 +55,7 @@ class CDate {
         friend ostream & operator<<(ostream & out, const CDate & date);
 };
 ostream & operator<<(ostream & out, const CDate & date) {
-    out << '{' << date.mY << ',' << (int)date.mM << ',' << (int)date.mD << '}';
+    out << '[' << date.mY << '-' << (int)date.mM << '-' << (int)date.mD << ']';
     return out;
 }
 
@@ -80,7 +81,7 @@ class StoreItem {
         friend ostream & operator<<(ostream & out, const StoreItem & item);
 };
 ostream & operator<<(ostream & out, const StoreItem & item) {
-    out << '(' << item.name << ',' << item.date << ',' << item.count << ')';
+    out << '(' << item.name << ", " << item.date << ", " << item.count << ')';
     return out;
 }
 
@@ -89,21 +90,26 @@ namespace cmp {
         bool operator()(const CDate & i1, const CDate & i2) const {
             return i1 < i2;
         }
-        bool operator()(const StoreItem & i1, const StoreItem & i2) const {
-            return i1.date < i2.date;
-        }
     };
     struct DateCmpInv {
-        bool operator()(const CDate & i1, const CDate & i2) const {
-            return i1 > i2;
-        }
         bool operator()(const StoreItem & i1, const StoreItem & i2) const {
-            return i1.date > i2.date;
+            if (i1.date > i2.date) return true;
+            if (i1.date < i2.date) return false;
+            // other for equals and lower_bound
+            if (i1.name > i2.name) return true;
+            if (i1.name < i2.name) return false;
+            if (i1.count < i2.count) return true;
+            if (i1.count > i2.count) return false;
+            return false;
         }
     };
     struct CountCmpInv {
         bool operator()(const NameCount & i1, const NameCount & i2) const {
-            return i1.count > i2.count;
+             if (i1.count > i2.count) return true;
+             if (i1.count < i2.count) return false;
+             if (i1.name > i2.name) return true;
+             if (i1.name < i2.name) return false;
+             return false;
         }
     };
 }
@@ -131,14 +137,13 @@ class CSupermarket {
             }
         }
 
-        mSet.insert(StoreItem(name, expiryDate, count));
+        insertOrUpdateToSet(name, expiryDate, count);
         return *this;
     }
 
-
     CSupermarket & sell(ProdList & shoppingList) {
         for (auto & item : shoppingList)
-            item.second = doBusyness(item.first, item.second);
+            item.second = doBusiness(item.first, item.second);
 
         while (shoppingList.size() != 0 && shoppingList.begin() -> second == 0)
             shoppingList.pop_front();
@@ -155,23 +160,31 @@ class CSupermarket {
         }
         return *this;
     }
-    inline int doBusyness(const string & name, int count) {
-        map<string, DateCountMap>::iterator dataPtr;
-        if (!findItem(name, dataPtr)) return count;
-        DateCountMap& data = dataPtr -> second;
+    private:
+    inline int doBusiness(const string & name, int count) {
+        map<string, DateCountMap>::iterator dataItr;
+        if (!findItem(name, dataItr)) return count;
+        const string& realName = dataItr -> first;
+        DateCountMap& data = dataItr -> second;
         vector<CDate> toRemove;
         for (auto &[key, value] : data) {
             if (value > count) {
                 value -= count;
                 count = 0;
+                updateInSet(realName, key, value);
                 break;
             } else {
                 count -= value;
                 toRemove.push_back(key);
             }
         }
-        for (const auto & key : toRemove)
+        for (const auto & key : toRemove) {
             data.erase(key);
+            updateInSet(realName, key, 0);
+        }
+        if (data.empty())
+            mMap.erase(dataItr);
+
         return count;
     }
     inline bool findItem(const string & name, map<string, DateCountMap>::iterator & mapOut) {
@@ -198,14 +211,37 @@ class CSupermarket {
         bool diffFound = false;
         for (size_t i = 0; i < len; i++) {
             if (str1[i] != str2[i]) {
-                if(diffFound) return false;
+                if(diffFound) {
+                    return false; 
+                }
                 diffFound = true;
             }
         }
         return true;
     }
+    void insertOrUpdateToSet(const string & name, const CDate & date, int count) {
+        auto setItr = mSet.lower_bound(StoreItem(name, date, 0));
+        if (setItr -> name == name && setItr -> date == date) {
+            StoreItem item = *setItr;
+            item.count += count;
+            mSet.erase(setItr);
+            mSet.insert(item);
+        } else {
+            mSet.insert(StoreItem(name, date, count));
+        }
+    }
+    void updateInSet(const string & name, const CDate & date, int count) {
+        auto setItr = mSet.lower_bound(StoreItem(name, date, 0));
+        StoreItem item = *setItr;
+        if (count != 0) {
+            item.count = count;
+            mSet.insert(item);
+        }
+        mSet.erase(setItr);
+    }
 
 
+    public:
     ProdList expired(const CDate & date) const {
         map<string, int> expired;
         set<NameCount, cmp::CountCmpInv> sorted;
@@ -226,47 +262,79 @@ class CSupermarket {
             outList.emplace_back(make_pair(item.name, item.count));
         return outList;
     }
-
-    private:
+    void printMap(ostream & out = cout) const {
+        cout << "Map" << endl;
+        for (const auto & [name, data] : mMap) {
+            cout << "+ " << name << endl;
+            for (const auto & [date, count] : data) {
+                cout << "> " << date << " - " << count << endl;
+            }
+        }
+    }
+    void printSet(ostream & out = cout) const {
+        cout << "Set" << endl;
+        for (const StoreItem & item : mSet) {
+            cout << "/ " << item << endl;
+        }
+    }
 };
 
 
 #ifndef __PROGTEST__
+void printLine(const string msg) {
+    cout << "--- " << setw(80 - 4) << setfill('-') << left << (msg + ' ') << endl;
+}
 void printList(const ProdList & list) {
-    cout << "> ";
+    cout << "- ";
     for (auto const & item : list) {
-        cout << "(" << item.first << " " << item.second << ") -> ";
+        cout << item.first << " " << item.second << " -> ";
     }
     cout << "null" << endl;
 }
 int main(void){
     CSupermarket s;
+
+    printLine("Init");
     s.store("bread", CDate(2016, 4, 30 ), 100 )
         .store("butter", CDate(2016, 5, 10 ), 10 )
         .store("beer", CDate(2016, 8, 10 ), 50 )
         .store("bread", CDate(2016, 4, 25 ), 100 )
         .store("okey", CDate(2016, 7, 18 ), 5 );
+    s.printMap();
+    s.printSet();
 
     list<pair<string,int>> l0 = s.expired(CDate(2018, 4, 30));
-    printList(l0);
     assert( l0.size () == 4 );
     assert((l0 == list<pair<string,int>> { { "bread", 200 }, { "beer", 50 }, { "butter", 10 }, { "okey", 5 } }));
 
     list<pair<string,int>> l1 { { "bread", 2 }, { "Coke", 5 }, { "butter", 20 } };
+    printLine("Sell 01");
+    printList(l1);
     s.sell(l1 );
+    printList(l1);
+    s.printMap();
+    s.printSet();
     assert( l1.size () == 2 );
     assert((l1 == list<pair<string,int>> { { "Coke", 5 }, { "butter", 10 } }));
 
     list<pair<string,int>> l2 = s.expired(CDate(2016, 4, 30));
+    printList(l2);
     assert( l2.size () == 1 );
     assert((l2 == list<pair<string,int>> { { "bread", 98 } }));
 
     list<pair<string,int>> l3 = s.expired(CDate(2016, 5, 20));
+    printList(l3);
+    assert((l2 == list<pair<string,int>> { { "bread", 98 } }));
     assert( l3.size () == 1 );
     assert((l3 == list<pair<string,int>> { { "bread", 198 } }));
 
     list<pair<string,int>> l4 { { "bread", 105 } };
+    printLine("Sell 04");
+    printList(l4);
     s.sell(l4 );
+    printList(l4);
+    s.printMap();
+    s.printSet();
     assert( l4.size () == 0 );
     assert((l4 == list<pair<string,int>> { }));
 
@@ -275,58 +343,100 @@ int main(void){
     assert((l5 == list<pair<string,int>> { { "bread", 93 }, { "beer", 50 }, { "okey", 5 } }));
 
     s.store("Coke", CDate(2016, 12, 31 ), 10 );
+    printLine("Stored Coke");
+    s.printMap();
+    s.printSet();
 
     list<pair<string,int>> l6 { { "Cake", 1 }, { "Coke", 1 }, { "cake", 1 }, { "coke", 1 }, { "cuke", 1 }, { "Cokes", 1 } };
+    printLine("Sell 06");
+    printList(l6);
     s.sell(l6 );
+    printList(l6);
+    s.printMap();
+    s.printSet();
     assert( l6.size () == 3 );
     assert((l6 == list<pair<string,int>> { { "cake", 1 }, { "cuke", 1 }, { "Cokes", 1 } }));
 
     list<pair<string,int>> l7 = s.expired(CDate(2017, 1, 1));
+    printList(l7);
     assert( l7.size () == 4 );
     assert((l7 == list<pair<string,int>> { { "bread", 93 }, { "beer", 50 }, { "Coke", 7 }, { "okey", 5 } }));
 
     s.store("cake", CDate(2016, 11, 1 ), 5 );
+    printLine("Stored cake");
 
     list<pair<string,int>> l8 { { "Cake", 1 }, { "Coke", 1 }, { "cake", 1 }, { "coke", 1 }, { "cuke", 1 } };
+    printLine("Sell 08");
+    printList(l8);
     s.sell(l8 );
+    printList(l8);
+    s.printMap();
+    s.printSet();
     assert( l8.size () == 2 );
     assert((l8 == list<pair<string,int>> { { "Cake", 1 }, { "coke", 1 } }));
 
     list<pair<string,int>> l9 = s.expired(CDate(2017, 1, 1));
+    printList(l9);
     assert( l9.size () == 5 );
     assert((l9 == list<pair<string,int>> { { "bread", 93 }, { "beer", 50 }, { "Coke", 6 }, { "okey", 5 }, { "cake", 3 } }));
 
     list<pair<string,int>> l10 { { "cake", 15 }, { "Cake", 2 } };
+    printLine("Sell 10");
+    printList(l10);
     s.sell(l10 );
+    printList(l10);
+    s.printMap();
+    s.printSet();
     assert( l10.size () == 2 );
     assert((l10 == list<pair<string,int>> { { "cake", 12 }, { "Cake", 2 } }));
 
     list<pair<string,int>> l11 = s.expired(CDate(2017, 1, 1));
+    printList(l11);
     assert( l11.size () == 4 );
     assert((l11 == list<pair<string,int>> { { "bread", 93 }, { "beer", 50 }, { "Coke", 6 }, { "okey", 5 } }));
 
     list<pair<string,int>> l12 { { "Cake", 4 } };
+    printLine("Sell 12");
+    printList(l12);
     s.sell(l12 );
+    printList(l12);
+    s.printMap();
+    s.printSet();
     assert( l12.size () == 0 );
     assert((l12 == list<pair<string,int>> { }));
 
     list<pair<string,int>> l13 = s.expired(CDate(2017, 1, 1));
+    printList(l13);
     assert( l13.size () == 4 );
     assert((l13 == list<pair<string,int>> { { "bread", 93 }, { "beer", 50 }, { "okey", 5 }, { "Coke", 2 } }));
 
     list<pair<string,int>> l14 { { "Beer", 20 }, { "Coke", 1 }, { "bear", 25 }, { "beer", 10 } };
+    printLine("Sell 14");
+    printList(l14);
     s.sell(l14 );
+    printList(l14);
+    s.printMap();
+    s.printSet();
     assert( l14.size () == 1 );
     assert((l14 == list<pair<string,int>> { { "beer", 5 } }));
 
     s.store("ccccb", CDate(2019, 3, 11 ), 100 )
         .store("ccccd", CDate(2019, 6, 9 ), 100 )
         .store("dcccc", CDate(2019, 2, 14 ), 100 );
+    printLine("Stored 2x ccccb, dcccc");
 
     list<pair<string,int>> l15 { { "ccccc", 10 } };
+    printLine("Sell 15");
+    printList(l15);
     s.sell(l15 );
+    printList(l15);
+    s.printMap();
+    s.printSet();
     assert( l15.size () == 1 );
     assert((l15 == list<pair<string,int>> { { "ccccc", 10 } }));
+
+    printLine("It's over, mr Frodo");
+    printLine("It's finally done!");
 
     return EXIT_SUCCESS;
 }
